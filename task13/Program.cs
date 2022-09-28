2189a9c86e7ec483dc8c6530590ac01d440afcea
NataliaPetrovaA <napetrov79@mail.ru>
2022-09-28 22:57:03 +0300

    task13

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,10 +1,10 @@
-﻿// task13
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 int GetNumber()
 {
     Console.WriteLine("Введите число!");
     return Convert.ToInt32(Console.ReadLine());     
 }
+
 void CheckHundreds(int number)
 {
     int сurrenе_number = number;
@@ -14,16 +14,13 @@ void CheckHundreds(int number)
     while(сurrenе_number > 999)
     {
       сurrenе_number = сurrenе_number /10;
-        // int result = сurrenе_number % 10;
     }
     Console.WriteLine("Третья цифра введенного числа " + сurrenе_number%10);  
     }
     else
     {
         Console.WriteLine("Третьей цифры НЕТ!");
-    }
-
-    
+    }  
 }
 
 int number = GetNumber();
