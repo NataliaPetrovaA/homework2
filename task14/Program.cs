aee3719d495a57e8465013155c80faccf6f72751
NataliaPetrovaA <napetrov79@mail.ru>
2022-09-28 22:56:10 +0300

    task14

--- task14/Program.cs
+++ task14/Program.cs
@@ -1,10 +1,21 @@
-﻿void task14()
-{// 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
-Console.WriteLine("Введите  число!");
-int number = Convert.ToInt32(Console.ReadLine());
-if (number % 161 != 0)
-Console.WriteLine($"число {number} не кратно числам 7 и 23 одновременно");
-else
-Console.WriteLine($"число {number} кратно числам 7 и 23 одновременно");
+﻿// Задача 14. Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+int GetNumber()
+{
+    Console.WriteLine("Введите число!");
+    return Convert.ToInt32(Console.ReadLine());     
 }
-task14();
+
+bool CheckMod(int a, int mod)
+{
+    return (a % mod == 0);
+}
+
+int a = GetNumber();
+if (CheckMod(a,7) && CheckMod(a,23))
+{
+    Console.WriteLine("Число кратно одновременно 7 и 23!");
+}
+else
+{
+    Console.WriteLine("Число не кратно одновременно 7 и 23!");
+}
